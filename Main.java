a6629959df00e1184a38c611fd40f4f70331fbd3
ddorshorst <ddorshorst@gmail.com>
2017-07-22 23:23:43 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
 
 public class Main {
-	/*public static void main(String[] args) {
-		StratTable strategy = new StratTable();
-		Date[] dates = {new Date("Jan", 1), new Date("Feb", 28)}; 
-		
-		strategy.update(dates, 2, 1);
-		strategy.update(dates, 2, 1);
-		strategy.update(dates, 2, 0);
-		
-		strategy.printStratTable();
-	}*/
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
